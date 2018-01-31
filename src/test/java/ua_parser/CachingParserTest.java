4b23d8da24e8c4cd60fa6e478a07bea8af56f54f
Dima Golomozy <dima.golomozy@gmail.com>
2018-01-31 15:48:58 +0200

    changed cached parser test

--- src/test/java/ua_parser/CachingParserTest.java
+++ src/test/java/ua_parser/CachingParserTest.java
@@ -1,10 +1,9 @@
 package ua_parser;
 
-import org.junit.Before;
+import com.google.common.cache.CacheStats;
+import com.google.common.cache.LoadingCache;
+import org.junit.Assert;
 import org.junit.Test;
-
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 
 /**
  * These tests really only redo the same tests as in ParserTest but with a
@@ -13,67 +12,55 @@ import java.io.InputStream;
  *
  * @author niels
  */
-public class CachingParserTest extends ParserTest {
-
-    @Before
-    public void initParser() throws Exception {
-        parser = new CachingParser();
-    }
-
-    @Override
-    Parser parserFromStringConfig(String configYamlAsString) throws Exception {
-        InputStream yamlInput = new ByteArrayInputStream(
-                configYamlAsString.getBytes("UTF8"));
-        return new CachingParser(yamlInput);
-    }
+public class CachingParserTest {
+    private final CachingParser onTest = new CachingParser();
 
     @Test
     public void testCachedParseUserAgent() {
-        super.testParseUserAgent();
-        super.testParseUserAgent();
-        super.testParseUserAgent();
+        String ua = "Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0";
+        onTest.parseUserAgent(ua);
+        onTest.parseUserAgent(ua);
+        onTest.parseUserAgent(ua);
+
+        assertCacheWorks(onTest.getCacheUserAgent(), 1, 2);
     }
 
     @Test
     public void testCachedParseOS() {
-        super.testParseOS();
-        super.testParseOS();
-        super.testParseOS();
+        String ua = "Mozilla/5.0 (Linux; Android 6.0; LEX626 Build/HBXCNCU5801811241S) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.98 Mobile Safari/537.36";
+        onTest.parseOS(ua);
+        onTest.parseOS(ua);
+        onTest.parseOS(ua);
+
+        assertCacheWorks(onTest.getCacheOS(), 1, 2);
     }
 
     @Test
     public void testCachedParseDevice() {
-        super.testParseDevice();
-        super.testParseDevice();
-        super.testParseDevice();
+        String ua = "Mozilla/5.0 (Linux; Android 7.0; PRIME X MAX 2018 Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.111 Mobile Safari/537.36";
+        onTest.parseDevice(ua);
+        onTest.parseDevice(ua);
+        onTest.parseDevice(ua);
+
+        assertCacheWorks(onTest.getCacheDevice(), 1, 2);
     }
 
     @Test
-    public void testCachedParseFirefox() {
-        super.testParseFirefox();
-        super.testParseFirefox();
-        super.testParseFirefox();
+    public void testCachedParse() {
+        String ua = "Mozilla/5.0 (Linux; Android 7.0; PRIME X MAX 2018 Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.111 Mobile Safari/537.36";
+        onTest.parse(ua);
+        onTest.parse(ua);
+        onTest.parse(ua);
+
+        assertCacheWorks(onTest.getCacheClient(), 1, 2);
+        assertCacheWorks(onTest.getCacheUserAgent(), 1, 0);
+        assertCacheWorks(onTest.getCacheDevice(), 1, 0);
+        assertCacheWorks(onTest.getCacheOS(), 1, 0);
     }
 
-    @Test
-    public void testCachedParsePGTS() {
-        super.testParsePGTS();
-        super.testParsePGTS();
-        super.testParsePGTS();
+    private void assertCacheWorks(LoadingCache<String, ?> cache, long loadCount, long hitCount) {
+        CacheStats stats = cache.stats();
+        Assert.assertEquals(loadCount, stats.loadCount());
+        Assert.assertEquals(hitCount, stats.hitCount());
     }
-
-    @Test
-    public void testCachedParseAll() {
-        super.testParseAll();
-        super.testParseAll();
-        super.testParseAll();
-    }
-
-    @Test
-    public void testCachedReplacementQuoting() throws Exception {
-        super.testReplacementQuoting();
-        super.testReplacementQuoting();
-        super.testReplacementQuoting();
-    }
-
 }
