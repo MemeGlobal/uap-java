b7755c0bf4dfd74f5a0185558cf56c83b0038e63
Dima Golomozy <dima.golomozy@gmail.com>
2018-01-31 10:38:32 +0200

    Dima/first init (#1)
    * settings file
    change pom
    
    * added regexes
    and test resources
    
    * indent all files
    
    * copy regexes file from sas

--- src/test/java/ua_parser/DataTest.java
+++ src/test/java/ua_parser/DataTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2012 Twitter, Inc
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,80 +32,80 @@ import static org.junit.Assert.*;
  * @author Steve Jiang (@sjiang) <gh at iamsteve com>
  */
 public abstract class DataTest<T> {
-  protected Random random;
-  protected Random seedRandom = new Random();
+    protected Random random;
+    protected Random seedRandom = new Random();
 
-  @Before
-  public void initialize() {
-    random = new Random();
-  }
-
-  protected abstract T getRandomInstance(long seed, StringGenerator g);
-
-  protected abstract T getBlankInstance();
-
-  protected long nextSeed() {
-    return seedRandom.nextLong();
-  }
-
-  protected class StringGenerator {
-    private HashSet<String> generated = new HashSet<String>();
-    private Random strRand;
-    protected final boolean unique;
-
-    public StringGenerator(long seed, boolean unique) {
-      strRand = new Random(seed);
-      this.unique = unique;
+    @Before
+    public void initialize() {
+        random = new Random();
     }
 
-    public String getString(int maxLen) {
-      if (!unique) {
-        return genString(maxLen);
-      }
-      while (true) {
-        String ret = genString(maxLen);
-        if (generated.add(ret)) {
-          return ret;
+    protected abstract T getRandomInstance(long seed, StringGenerator g);
+
+    protected abstract T getBlankInstance();
+
+    protected long nextSeed() {
+        return seedRandom.nextLong();
+    }
+
+    protected class StringGenerator {
+        private HashSet<String> generated = new HashSet<String>();
+        private Random strRand;
+        protected final boolean unique;
+
+        public StringGenerator(long seed, boolean unique) {
+            strRand = new Random(seed);
+            this.unique = unique;
         }
-      }
+
+        public String getString(int maxLen) {
+            if (!unique) {
+                return genString(maxLen);
+            }
+            while (true) {
+                String ret = genString(maxLen);
+                if (generated.add(ret)) {
+                    return ret;
+                }
+            }
+        }
+
+        private String genString(int maxLen) {
+            int len = strRand.nextInt(maxLen + 1);
+            StringBuilder sb = new StringBuilder(len);
+            for (int i = len; i-- > 0; ) {
+                // ascii printable range 32 to 126
+                sb.append((char) (strRand.nextInt(126 - 32 + 1) + 32));
+            }
+            return sb.toString();
+        }
     }
 
-    private String genString(int maxLen) {
-      int len = strRand.nextInt(maxLen + 1);
-      StringBuilder sb = new StringBuilder(len);
-      for (int i = len; i-- > 0; ) {
-        // ascii printable range 32 to 126
-        sb.append((char)(strRand.nextInt(126-32+1)+32));
-      }
-      return sb.toString();
+    @Test
+    public void testEqualsAndHashCode() {
+        for (int i = 1000; i-- > 0; ) {
+            long seed = nextSeed();
+            T first = getRandomInstance(seed, new StringGenerator(seed, false));
+            T second = getRandomInstance(seed, new StringGenerator(seed, false));
+            assertThat(first, is(second));
+            assertThat(first.hashCode(), is(second.hashCode()));
+        }
     }
-  }
 
-  @Test
-  public void testEqualsAndHashCode() {
-    for (int i = 1000; i-- > 0; ) {
-      long seed = nextSeed();
-      T first = getRandomInstance(seed, new StringGenerator(seed, false));
-      T second = getRandomInstance(seed, new StringGenerator(seed, false));
-      assertThat(first, is(second));
-      assertThat(first.hashCode(), is(second.hashCode()));
+    @Test
+    public void testNotEquals() {
+        for (int i = 1000; i-- > 0; ) {
+            StringGenerator uniqueGenerator = new StringGenerator(nextSeed(), true);
+            T first = getRandomInstance(nextSeed(), uniqueGenerator);
+            T second = getRandomInstance(nextSeed(), uniqueGenerator);
+            assertThat(first, is(not(second)));
+        }
     }
-  }
 
-  @Test
-  public void testNotEquals() {
-    for (int i = 1000; i-- > 0; ) {
-      StringGenerator uniqueGenerator = new StringGenerator(nextSeed(), true);
-      T first = getRandomInstance(nextSeed(), uniqueGenerator);
-      T second = getRandomInstance(nextSeed(), uniqueGenerator);
-      assertThat(first, is(not(second)));
+    @Test
+    public void testBlankInstances() {
+        T first = getBlankInstance(), second = getBlankInstance();
+        assertThat(first, is(second));
+        assertThat(first.hashCode(), is(second.hashCode()));
     }
-  }
-
-  @Test
-  public void testBlankInstances() {
-    T first = getBlankInstance(), second = getBlankInstance();
-    assertThat(first, is(second));
-    assertThat(first.hashCode(), is(second.hashCode()));
-  }
 }
